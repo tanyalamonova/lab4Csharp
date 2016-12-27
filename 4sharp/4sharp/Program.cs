976b01c8c8f56fded66b951c60c6ba5a8a00228b
Татьяна Ламонова <t.s.lamonova@gmail.com>
2016-12-27 16:56:13 +0700

    lab 4 C#
    the fourth kab on C#

---
+++ 4sharp/4sharp/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+
+namespace _4sharp
+{
+    class Program
+    {
+        class Token
+        {
+            public string data;
+            public int recipient;
+
+            public Token(string text, int number)
+            {
+                data = text;
+                recipient = number;
+            }
+        }
+
+        static void Opener(int index, Token token)
+        {
+
+            if (index < token.recipient)
+            {
+                Console.WriteLine("thread #" + index.ToString() + " got a token");
+                Thread thread = new Thread(() => Opener(index + 1, token));
+                thread.Start();
+            }
+            else
+            {
+                Console.WriteLine("thread #" + index.ToString() + " received a token: " + token.data);
+            }
+            //Console.WriteLine("thread #" + index.ToString() + " ended");
+        }
+
+        static void Main(string[] args)
+        {
+
+            Token token = new Token("ho ho ho!", 3000);
+            Opener(1, token);
+            
+            Console.Read();
+
+        }
+     }
+}
